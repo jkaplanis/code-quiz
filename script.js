9252aacda5cd5691622e810479c161ce9fce0ef9
jkaplanis <johntkaplanis@gmail.com>
2020-03-16 18:43:51 -0700

    Completed quiz loops, built submit initials page, saved user initials to local storage.

--- script.js
+++ script.js
@@ -1,264 +1,111 @@
-// Create a timed quiz on JavaDcript fundamentals that stores high scores
-
-
-var h1El = document.querySelector("#main-text");
-var subtext = document.querySelector("#subtext");
-var buttonWrapper = document.querySelector("#button-div");
-var startQuiz = document.querySelector("#start-quiz");
-var quizWrapper = document.querySelector("#quiz-wrapper");
-var correctAnswer;
-var incorrectAnswer;
+var currentQuestion = 0;
 var score = 0;
 
-// var questionArray = [
-//     [0, "The question", "Answer1", "Answer3"...]
-// ]
+var questionsArray = [
+    {
+        question: "This is the first question",
+        choices: ["choice 1", "choice 2 correct", "choice 3", "choice 4"],
+        answer: 1
+    },
+    {
+        question: "This is the second question",
+        choices: ["choice 1 correct", "choice 2", "choice 3", "choice 4"],
+        answer: 0
+    },
+    {
+        question: "This is the third question",
+        choices: ["choice 1", "choice 2", "choice 3", "choice 4 correct"],
+        answer: 3
+    },
+    {
+        question: "This is the fourth question",
+        choices: ["choice 1", "choice 2", "choice 3", "choice 4 correct"],
+        answer: 3
+    },
+    {
+        question: "This is the fifth question",
+        choices: ["choice 1", "choice 2", "choice 3 correct", "choice 4"],
+        answer: 2
+    }
+];
 
-// var questionArray = [
-//     {
-//         answer: 0,
-//         question: 'What is something',
-//         answers: ['answer1', 'answer2', 'answer3', 'answer4']
-//     },
-//     {}
 
-    // Loop through answers and make an element
-    // Each answer element will have a data attribute equal to its attay position data-index="0"
-    // append element to the screen
-    // When answer is clicked if data-index === answer then it is correct else its wrong
-// ]
+var startQuiz = document.querySelector("#start-quiz");
 
-// These are the buttons for the quiz
-var ans1 = document.createElement("button");
-var ans2 = document.createElement("button");
-var ans3 = document.createElement("button");
-var ans4 = document.createElement("button");
-var ans1_2 = document.createElement("button");
-var ans2_2 = document.createElement("button");
-var ans3_2 = document.createElement("button");
-var ans4_2 = document.createElement("button");
-var ans1_3 = document.createElement("button");
-var ans2_3 = document.createElement("button");
-var ans3_3 = document.createElement("button");
-var ans4_3 = document.createElement("button");
-var ans1_4 = document.createElement("button");
-var ans2_4 = document.createElement("button");
-var ans3_4 = document.createElement("button");
-var ans4_4 = document.createElement("button");
-var ans1_5 = document.createElement("button");
-var ans2_5 = document.createElement("button");
-var ans3_5 = document.createElement("button");
-var ans4_5 = document.createElement("button");
-// **The below should be in a function that is called at the beginning of this js file. 
-// When the page is loaded, the h1 element should be populated with the title and the p element should have the description. 
-// a button should be created (createElement and appendChild) that has the start button
-
-// When the start button is clicked 
-// Create an addEventLister "click" on a variable holding my play button element
 startQuiz.addEventListener("click", function () {
     event.stopPropagation();
-    question1();
+    var startWrapper = document.querySelector("#start-wrapper");
+    startWrapper.innerHTML = "";
+    questionSetUp();
 });
 
-// This is the correct function
-function correct() {
-    // adds a p tag with correct
-    correctAnswer = document.createElement("p");
-    quizWrapper.appendChild(correctAnswer);
-    correctAnswer.textContent = "Correct!";
-    score++;
-    // removes the p tag with correct after 2 seconds. 
-    var displayCorrect = setInterval(function () {
-        correctAnswer.parentNode.removeChild(correctAnswer);
-        clearInterval(displayCorrect);
-    }, 2000);
-    console.log(score);
+
+var quizWrapper = document.querySelector("#quiz-question");
+var questionEl = document.querySelector("#quiz-question");
+var choiceButtons = document.querySelector("#button-wrapper");
+
+var choicesArray = [];
+
+function questionSetUp() {
+    if (currentQuestion <= (questionsArray.length -1)){
+
+        questionEl.textContent = questionsArray[currentQuestion].question;
+        for (var i = 0; i < questionsArray[currentQuestion].choices.length; i++) {
+            var btn = document.createElement("button");
+            btn.setAttribute("data-index", i);
+            btn.textContent = questionsArray[currentQuestion].choices[i];
+            choicesArray.push(btn);
+            choiceButtons.appendChild(choicesArray[i]);
+        };
+    }
+    else {
+        allDone();
+    }
 }
 
-// This is the incorrect function
-function incorrect() {
-    // adds a p tag with incorrect correct
-    incorrectAnswer = document.createElement("p");
-    quizWrapper.appendChild(incorrectAnswer);
-    incorrectAnswer.textContent = "Incorrect!";
-    // removes the p tag with incorrect after 2 seconds. 
-    var displayIncorrect = setInterval(function () {
-        incorrectAnswer.parentNode.removeChild(incorrectAnswer);
-        clearInterval(displayIncorrect);
-    }, 2000);
+// if the button's data-index is === to answer then the its correct, else its incorrect 
+choiceButtons.addEventListener("click", function (event) {
+
+    if (parseInt(event.target.getAttribute("data-index")) === questionsArray[currentQuestion].answer) {
+        alert("correct");
+        score += 1;
+        clearChoices();
+        currentQuestion += 1;
+        questionSetUp();
+
+    }
+    else {
+        alert("incorrect");
+        clearChoices();
+        currentQuestion += 1;
+        questionSetUp();
+    }
+});
+
+
+function clearChoices() {
+    while (choiceButtons.firstChild) {
+        choiceButtons.removeChild(choiceButtons.firstChild);
+    }
+    choicesArray = [];
+    questionEl.textContent = "";
 }
 
-// Then a timer starts and I am presented with a question
-function question1() {
-    // content for first question
-    h1El.textContent = "What is javascript?";
-    // remove subtext and start quiz button
-    subtext.parentNode.removeChild(subtext);
-    startQuiz.parentNode.removeChild(startQuiz);
-    // add answer buttons
-    buttonWrapper.appendChild(ans1);
-    buttonWrapper.appendChild(ans2);
-    buttonWrapper.appendChild(ans3);
-    buttonWrapper.appendChild(ans4);
-    // add content to answer buttons
-    ans1.textContent = "answer 1 wrong";
-    ans2.textContent = "answer 2 correct";
-    ans3.textContent = "answer 3 wrong";
-    ans4.textContent = "answer 4 wrong";
-    // check to see if they clicked the correct or incorrect answer
-    
-    buttonWrapper.addEventListener("click", function () {
-        if (ans2) {
-            question2();
-            correct();
-        }
-        else if (ans1 || ans3 || ans4){
-            question2();
-            incorrect();
-        }
-    });
-    return;
-};
-
-// When a question is answered
-// Then another question is presented
-function question2() {
-    // content for question
-    h1El.textContent = "This is question 2?";
-    // remove question 1 buttons
-    buttonWrapper.removeChild(ans1);
-    buttonWrapper.removeChild(ans2);
-    buttonWrapper.removeChild(ans3);
-    buttonWrapper.removeChild(ans4);
-    // add answer buttons
-    buttonWrapper.appendChild(ans1_2);
-    buttonWrapper.appendChild(ans2_2);
-    buttonWrapper.appendChild(ans3_2);
-    buttonWrapper.appendChild(ans4_2);
-    // add content to answer buttons
-    ans1_2.textContent = "answer 1 wrong";
-    ans2_2.textContent = "answer 2 wrong";
-    ans3_2.textContent = "answer 3 wrong";
-    ans4_2.textContent = "answer 4 correct";
-    // check to see if they clicked the correct or incorrect answer
-    buttonWrapper.addEventListener("click", function () {
-        if (ans4_2) {
-            question3();
-            correct();
-        }
-        else if (ans1_2 || ans2_2 || ans3_2){
-            question3();
-            incorrect();
-        }
-    });
-    return;
-};
-
-// question 3
-function question3() {
-    // content for question
-    h1El.textContent = "This is question 3?";
-    // remove question 1 buttons
-    ans1_2.parentNode.removeChild(ans1_2);
-    ans2_2.parentNode.removeChild(ans2_2);
-    ans3_2.parentNode.removeChild(ans3_2);
-    ans4_2.parentNode.removeChild(ans4_2);
-    // add answer buttons
-    buttonWrapper.appendChild(ans1_3);
-    buttonWrapper.appendChild(ans2_3);
-    buttonWrapper.appendChild(ans3_3);
-    buttonWrapper.appendChild(ans4_3);
-    // add content to answer buttons
-    ans1_3.textContent = "answer 1 correct";
-    ans2_3.textContent = "answer 2 wrong";
-    ans3_3.textContent = "answer 3 wrong";
-    ans4_3.textContent = "answer 4 wrong";
-    // check to see if they clicked the correct or incorrect answer
-    buttonWrapper.addEventListener("click", function () {
-        if (ans1_3) {
-            question4();
-            correct();
-        }
-        else if (ans2_3 || ans3_3 || ans4_3){
-            question4();
-            incorrect();
-        }
-    });
-    return;
-};
-// question 4
-function question4() {
-    // content for question
-    h1El.textContent = "This is question 4?";
-    // remove question 1 buttons
-    ans1_3.parentNode.removeChild(ans1_3);
-    ans2_3.parentNode.removeChild(ans2_3);
-    ans3_3.parentNode.removeChild(ans3_3);
-    ans4_3.parentNode.removeChild(ans4_3);
-    // add answer buttons
-    buttonWrapper.appendChild(ans1_4);
-    buttonWrapper.appendChild(ans2_4);
-    buttonWrapper.appendChild(ans3_4);
-    buttonWrapper.appendChild(ans4_4);
-    // add content to answer buttons
-    ans1_4.textContent = "answer 1 correct";
-    ans2_4.textContent = "answer 2 wrong";
-    ans3_4.textContent = "answer 3 wrong";
-    ans4_4.textContent = "answer 4 wrong";
-    // check to see if they clicked the correct or incorrect answer
-    buttonWrapper.addEventListener("click", function () {
-        if (ans1_4) {
-            question5();
-            correct();
-        }
-        else if (ans2_4 || ans3_4 || ans4_4){
-            question5();
-            incorrect();
-        }
-    });
-    return;
-};
-
-// question 5
-function question5() {
-    // content for question
-    h1El.textContent = "This is question 5?";
-    // remove question 1 buttons
-    ans1_4.parentNode.removeChild(ans1_4);
-    ans2_4.parentNode.removeChild(ans2_4);
-    ans3_4.parentNode.removeChild(ans3_4);
-    ans4_4.parentNode.removeChild(ans4_4);
-    // add answer buttons
-    buttonWrapper.appendChild(ans1_5);
-    buttonWrapper.appendChild(ans2_5);
-    buttonWrapper.appendChild(ans3_5);
-    buttonWrapper.appendChild(ans4_5);
-    // add content to answer buttons
-    ans1_5.textContent = "answer 1 wrong";
-    ans2_5.textContent = "answer 2 correct";
-    ans3_5.textContent = "answer 3 wrong";
-    ans4_5.textContent = "answer 4 wrong";
-    // check to see if they clicked the correct or incorrect answer
-    buttonWrapper.addEventListener("click", function () {
-        if (ans2_5) {
-            // question2();
-            correct();
-        }
-        else if (ans1_5 || ans3_5 || ans4_5){
-            incorrect();
-        }
-    });
-    return;
-};
+var submit = document.querySelector("#submit");
+var initialsInput = document.querySelector("#initials");
 
 function allDone(){
-
+    questionEl.textContent = "All Done!";
+    
+    var yourScore = document.createElement("p");
+    yourScore.textContent = ("Your final score is " + score);
+    questionEl.appendChild(yourScore);
+    
+    var initialsForm = document.querySelector(".input-group");
+    initialsForm.setAttribute("class", "input-group");
 }
-// When a question is answered incorrectly
-// Then a 10second penalty is applied, meaning the timer should sudenly jump but 10 more seconds
-
-// When all questions are answered or the timer reaches 0
-// Then the game is over
-
-// When the game is over
-// Then I can save my initials and score
+submit.addEventListener("click", function(event){
+    var initials = initialsInput.value.trim();
+    event.preventDefault();
+    localStorage.setItem("initials", JSON.stringify(initials));
+})
